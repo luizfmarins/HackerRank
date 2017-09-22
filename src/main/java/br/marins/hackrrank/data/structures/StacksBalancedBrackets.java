ca9763403b042537fb3071835685c775f653efae
luiz.marins <luiz.f.marins@gmail.com>
2017-09-22 20:38:31 -0300

    [Stacks Balanced Brackets] Solves the sample problem

--- src/main/java/br/marins/hackrrank/data/structures/StacksBalancedBrackets.java
+++ src/main/java/br/marins/hackrrank/data/structures/StacksBalancedBrackets.java
@@ -1,10 +1,84 @@
 package br.marins.hackrrank.data.structures;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class StacksBalancedBrackets {
 
-  public String isBalanced(InputStream input) {
+  private static final String OPEN_BRACKETS = "{[(";
+
+  private Stack<String> stack;
+
+  public List<String> isBalanced(InputStream input) throws IOException {
+    List<String> lines = readLines(input);
+    List<String> values = getValues(lines);
+    List<String> ret = new ArrayList<>();
+
+    values.forEach(v -> ret.add(isBalanced(v)));
+
+    return ret;
+  }
+
+  private String isBalanced(String value) {
+    stack = new Stack<>();
+    char[] chars = value.toCharArray();
+
+    for (int i = 0; i < chars.length; i++) {
+      String currentBracket = String.valueOf(chars[i]);
+      if (isOpenBracket(currentBracket)) {
+        stack.push(currentBracket);
+      } else if (!matchesPreviousBracket(currentBracket)) {
+        return "NO";
+      }
+    }
+
     return "YES";
+  }
+
+  private boolean isOpenBracket(String bracket) {
+    return OPEN_BRACKETS.contains(bracket);
+  }
+
+  private boolean matchesPreviousBracket(String currentBracket) {
+    String openBracket = stack.pop();
+
+    return "(".equals(openBracket) && ")".equals(currentBracket)
+        || "[".equals(openBracket) && "]".equals(currentBracket)
+        || "{".equals(openBracket) && "}".equals(currentBracket);
+  }
+
+  private List<String> getValues(List<String> lines) {
+    List<String> values = new ArrayList<>();
+
+    for (int i = 1; i <= getNumberOfLines(lines); i++) {
+      values.add(lines.get(i));
+    }
+
+    return values;
+  }
+
+  private int getNumberOfLines(List<String> lines) {
+    return Integer.parseInt(getMetadata(lines));
+  }
+
+  private String getMetadata(List<String> lines) {
+    return lines.get(0);
+  }
+
+  private List<String> readLines(InputStream input) throws IOException {
+    List<String> lines = new ArrayList<>();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+    String line;
+    while ((line = reader.readLine()) != null) {
+      lines.add(line);
+    }
+
+    return lines;
   }
 }
--- src/test/java/br/marins/hackrrank/data/structures/StacksBalancedBracketsTest.java
+++ src/test/java/br/marins/hackrrank/data/structures/StacksBalancedBracketsTest.java
@@ -10,29 +10,37 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.List;
 import org.junit.Test;
 
 public class StacksBalancedBracketsTest {
 
   public static final String YES = "YES";
+  public static final String NO = "NO";
   private StacksBalancedBrackets sut = new StacksBalancedBrackets();
 
   @Test
   public void test() throws Exception {
-    InputStream input = input("1", "{[()]}");
+    InputStream input = input(
+        "3",
+        "{[()]}",
+        "{[(])}",
+        "{{[[(())]]}}");
 
-    String isBalanced = sut.isBalanced(input);
+    List<String> result = sut.isBalanced(input);
 
-    assertThat(isBalanced, equalTo(YES));
+    assertThat(result.get(0), equalTo(YES));
+    assertThat(result.get(1), equalTo(NO));
+    assertThat(result.get(2), equalTo(YES));
   }
 
   private InputStream input(String line1, String... nextLines) throws IOException {
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     PrintStream printer = new PrintStream(out);
 
-    write(line1, printer, defaultCharset());
+    write(line1 + "\n", printer, defaultCharset());
     for (int i = 0; i < nextLines.length; i++) {
-      write(nextLines[i], printer, defaultCharset());
+      write(nextLines[i] + "\n", printer, defaultCharset());
     }
 
     printer.close();
