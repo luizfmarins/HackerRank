93a6dc6c540308ffc3b953730ef84275f35a9494
luiz.marins <luiz.f.marins@gmail.com>
2017-10-01 15:02:53 -0300

    [Trees: Is This a Binary Search Tree?] Solve sample test case

--- src/main/java/br/marins/hacker/rank/data/structures/TreesIsThisBinarySearchTree.java
+++ src/main/java/br/marins/hacker/rank/data/structures/TreesIsThisBinarySearchTree.java
@@ -3,7 +3,20 @@ package br.marins.hacker.rank.data.structures;
 public class TreesIsThisBinarySearchTree {
 
   boolean checkBST(Node root) {
-    return false;
+    if (root == null) {
+      return true;
+    }
+
+    if (root.left != null && root.data < root.left.data) {
+      return false;
+    }
+
+    if (root.right != null && root.data > root.right.data) {
+      return false;
+    }
+
+    return checkBST(root.left)
+        && checkBST(root.right);
   }
 }
 
