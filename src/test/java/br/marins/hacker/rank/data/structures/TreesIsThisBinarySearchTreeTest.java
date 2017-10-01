a41d1ca957895bea37ccf0b98b8a7dfabc1b87f4
luiz.marins <luiz.f.marins@gmail.com>
2017-10-01 14:55:45 -0300

    [Trees: Is This a Binary Search Tree?] Create sample test case

--- src/test/java/br/marins/hacker/rank/data/structures/TreesIsThisBinarySearchTreeTest.java
+++ src/test/java/br/marins/hacker/rank/data/structures/TreesIsThisBinarySearchTreeTest.java
@@ -1,12 +1,41 @@
 package br.marins.hacker.rank.data.structures;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class TreesIsThisBinarySearchTreeTest {
 
+  private TreesIsThisBinarySearchTree sut = new TreesIsThisBinarySearchTree();
+
   @Test
   public void sampleTest() {
+    Node tree = newNode(4,
+        newNode(2,
+            newNode(1),
+            newNode(3)
+        ),
+        newNode(6,
+            newNode(5),
+            newNode(7)
+        )
+    );
 
+    boolean isBinarySearch = sut.checkBST(tree);
+
+    assertTrue(isBinarySearch);
   }
 
+  private Node newNode(int data, Node left, Node right) {
+    Node node = newNode(data);
+    node.left = left;
+    node.right = right;
+    return node;
+  }
+
+  private Node newNode(int data) {
+    Node node = new Node();
+    node.data = data;
+    return node;
+  }
 }
