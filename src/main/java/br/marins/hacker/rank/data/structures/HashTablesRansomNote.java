efda86b6f9c5f5f804700378d2688e17738112f1
luiz.marins <luiz.f.marins@gmail.com>
2017-10-02 22:26:30 -0300

    [Hash Tables: Ransom Note] Solve sample test case with simplest solution

--- src/main/java/br/marins/hacker/rank/data/structures/HashTablesRansomNote.java
+++ src/main/java/br/marins/hacker/rank/data/structures/HashTablesRansomNote.java
@@ -1,11 +1,54 @@
 package br.marins.hacker.rank.data.structures;
 
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HashTablesRansomNote {
 
-  public boolean canUseTheMagazine(InputStream input) {
+  public boolean canUseTheMagazine(InputStream input) throws IOException {
+    List<String> lines = readLines(input);
+    String[] wordsMagazine = getWordsMagazine(lines);
+    String[] wordsNote = getWordsNote(lines);
 
-    return false;
+    return canUseTheMagazine(wordsNote, wordsMagazine);
   }
+
+  private boolean canUseTheMagazine(String[] wordsNote, String[] wordsMagazine) {
+    List<String> listWordsNote = asList(wordsNote);
+    List<String> listWordsMagazine = asList(wordsMagazine);
+
+    boolean isMissingWord = listWordsNote.stream().filter(w -> !listWordsMagazine.contains(w))
+        .findFirst()
+        .isPresent();
+
+    return !isMissingWord;
+  }
+
+  private String[] getWordsMagazine(List<String> lines) {
+    return lines.get(1).split(SEPARATOR);
+  }
+
+  private String[] getWordsNote(List<String> lines) {
+    return lines.get(2).split(SEPARATOR);
+  }
+
+  private List<String> readLines(InputStream input) throws IOException {
+    List<String> lines = new ArrayList<>();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+    String line;
+    while ((line = reader.readLine()) != null) {
+      lines.add(line);
+    }
+
+    return lines;
+  }
+
+  public static final String SEPARATOR = " ";
 }
