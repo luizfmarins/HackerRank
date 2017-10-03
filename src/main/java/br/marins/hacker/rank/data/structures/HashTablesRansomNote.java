1c7a9a247ff3fd3af3c2984b68288ce42950277e
luiz.marins <luiz.f.marins@gmail.com>
2017-10-02 22:45:00 -0300

    [Hash Tables: Ransom Note] Create main class to run in HackerRank

--- src/main/java/br/marins/hacker/rank/data/structures/HashTablesRansomNote.java
+++ src/main/java/br/marins/hacker/rank/data/structures/HashTablesRansomNote.java
@@ -1,15 +1,23 @@
 package br.marins.hacker.rank.data.structures;
 
-import static java.util.Arrays.asList;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class HashTablesRansomNote {
+
+  public static void main(String[] args) throws IOException {
+    InputStream in = System.in;
+
+    HashTablesRansomNote note = new HashTablesRansomNote();
+    boolean canUseTheMagazine = note.canUseTheMagazine(in);
+
+    System.out.println(canUseTheMagazine ? "Yes" : "No");
+  }
 
   public boolean canUseTheMagazine(InputStream input) throws IOException {
     List<String> lines = readLines(input);
@@ -20,8 +28,8 @@ public class HashTablesRansomNote {
   }
 
   private boolean canUseTheMagazine(String[] wordsNote, String[] wordsMagazine) {
-    List<String> listWordsNote = asList(wordsNote);
-    List<String> listWordsMagazine = new ArrayList<>(asList(wordsMagazine));
+    List<String> listWordsNote = Arrays.asList(wordsNote);
+    List<String> listWordsMagazine = new ArrayList<>(Arrays.asList(wordsMagazine));
 
     boolean isMissingWord = listWordsNote.stream().filter(w -> {
       return !listWordsMagazine.remove(w);
