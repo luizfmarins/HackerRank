33f7cd072709b687170ed0c01dfeea92b7dd7e14
luiz.marins <luiz.marins@liferay.com>
2017-12-29 18:38:04 -0200

    [Linked Lists: Detect a Cycle] Improve performance

--- src/main/java/br/marins/hacker/rank/data/structures/LinkedListsDetectCycle.java
+++ src/main/java/br/marins/hacker/rank/data/structures/LinkedListsDetectCycle.java
@@ -1,25 +1,40 @@
 package br.marins.hacker.rank.data.structures;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LinkedListsDetectCycle {
 
   public boolean hasCycle(Node node) {
-    Set<Node> nodesVisited = new HashSet<>(); // TODO Node do not implement hashCode
+    Map<Integer, Set<Node>> nodesVisited = new HashMap<>();
 
     return hasCycle(node, nodesVisited);
   }
 
-  private boolean hasCycle(Node node, Set<Node> nodesVisited) {
+  private boolean hasCycle(Node node, Map<Integer, Set<Node>> nodesVisited) {
     if (node == null)
       return false;
 
-    boolean isNewNode = nodesVisited.add(node);
+    boolean isNewNode = add(nodesVisited, node);
     if (!isNewNode)
       return true;
 
     return hasCycle(node.next, nodesVisited);
+  }
+
+  private boolean add(Map<Integer, Set<Node>> nodes, Node node) {
+    Set<Node> nodesWithSameData = getNodes(nodes, node.data);
+
+    return nodesWithSameData.add(node);
+  }
+
+  private Set<Node> getNodes(Map<Integer, Set<Node>> nodes, int data) {
+    if (!nodes.containsKey(data))
+      nodes.put(data, new HashSet<Node>());
+
+    return nodes.get(data);
   }
 
   static class Node {
