8194f0087883696a8682e85e023aa12296022ef6
luiz.marins <luiz.marins@liferay.com>
2017-12-29 18:18:12 -0200

    [Linked Lists: Detect a Cycle] Solution 1

--- src/main/java/br/marins/hacker/rank/data/structures/LinkedListsDetectCycle.java
+++ src/main/java/br/marins/hacker/rank/data/structures/LinkedListsDetectCycle.java
@@ -1,13 +1,35 @@
 package br.marins.hacker.rank.data.structures;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LinkedListsDetectCycle {
 
-  public boolean hasCycle(Node head) {
-    return false;
+  private final Set<Integer> nodesVisited = new HashSet<>();
+
+  public boolean hasCycle(Node node) {
+    if (node == null)
+      return false;
+
+    boolean isNewNode = nodesVisited.add(node.data);
+    if (!isNewNode)
+      return true;
+
+    return hasCycle(node.next);
   }
 
   static class Node {
+
     int data;
     Node next;
+
+    public Node(int data) {
+      this.data = data;
+    }
+
+    public Node(int data, Node next) {
+      this.data = data;
+      this.next = next;
+    }
   }
 }
--- src/test/java/br/marins/hacker/rank/data/structures/LinkedListsDetectCycleTest.java
+++ src/test/java/br/marins/hacker/rank/data/structures/LinkedListsDetectCycleTest.java
@@ -1,7 +1,61 @@
 package br.marins.hacker.rank.data.structures;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import br.marins.hacker.rank.data.structures.LinkedListsDetectCycle.Node;
+import org.junit.Test;
+
 public class LinkedListsDetectCycleTest {
 
+  private static final Node EMPTY_LIST = null;
 
+  private LinkedListsDetectCycle sut = new LinkedListsDetectCycle();
 
+  @Test
+  public void emptyList() {
+    boolean hasCycle = sut.hasCycle(EMPTY_LIST);
+
+    assertThat(hasCycle, is(false));
+  }
+
+  @Test
+  public void onlyOneElement_noCycle() {
+    boolean hasCycle = sut.hasCycle(new Node(1));
+
+    assertThat(hasCycle, is(false));
+  }
+
+  @Test
+  public void onlyOneElement_withCycle() {
+    Node node = new Node(1);
+    node.next = node;
+
+    boolean hasCycle = sut.hasCycle(node);
+
+    assertThat(hasCycle, is(true));
+  }
+
+  @Test
+  public void twoOneElement_withCycle() {
+    Node second = new Node(2);
+    Node head = new Node(1, second);
+    second.next = head;
+
+    boolean hasCycle = sut.hasCycle(head);
+
+    assertThat(hasCycle, is(true));
+  }
+
+  @Test
+  public void TreeOneElement_withCycle() {
+    Node third = new Node(3);
+    Node second = new Node(2, third);
+    Node head = new Node(1, second);
+    third.next = second;
+
+    boolean hasCycle = sut.hasCycle(head);
+
+    assertThat(hasCycle, is(true));
+  }
 }
